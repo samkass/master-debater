3493710ad4f3e516bd3fe5cacdfaa640eaf88c85
Sam Kass <samkass@samkass.com>
2023-08-17 19:26:48 +0200

    Ask debater to incorporate persona into arguments.

--- Persona.py
+++ Persona.py
@@ -40,6 +40,7 @@ class Persona:
         You do not have to use all the adjectives if they are not relevant to your argument or the topic.
         Reply as if you were the persona, without any editorializing. If you are a historical figure, try to imitate
         their point of view and style of speaking.
+        Utilize aspects of your persona's identity in formulating your arguments.
         '''
         if prompt != "":
             remarks += f'''
@@ -56,6 +57,9 @@ class Persona:
         specific examples and citing statistics when possible: {prompt}
         Try to limit the length of your statement to 3 paragraphs or less.
         You do not have to use all the adjectives if they are not relevant to your argument or the topic.
+        Reply as if you were the persona, without any editorializing. If you are a historical figure, try to imitate
+        their point of view and style of speaking.
+        Utilize aspects of your persona's identity in formulating your arguments.
         '''
 
     def conclusion_prompt(self, topic="", prompt=""):
